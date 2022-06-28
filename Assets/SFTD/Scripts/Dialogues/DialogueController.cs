b8155138be7cb40ef0c588460b45e42cbfd9ea2d
NeilKleistGao <neilkleist@gmail.com>
2022-06-28 09:56:02 +0800

    Add show and hide

--- Assets/SFTD/Scripts/Dialogues/DialogueController.cs
+++ Assets/SFTD/Scripts/Dialogues/DialogueController.cs
@@ -2,17 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DialogueController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class DialogueController : MonoBehaviour {
+    [SerializeField] private float defaultSpeed = 1.0f;
+    [SerializeField] private Canvas mDialogueCanvas;
+
+    private static DialogueController instance;
+
+    private void Assert<T>(T obj, string name) {
+        if (obj == null) {
+            Debug.LogErrorFormat("Error: {0} not found.", name);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Awake() {
+        instance = this;
+        Assert(mDialogueCanvas, "Dialogue Canvas");
+    }
+
+    public static DialogueController Instance { 
+        get { return instance; }
+    }
+
+    private void Start() {
+        // for test only
+        StartDialogue();
+    }
+
+    public void StartDialogue() {
+        mDialogueCanvas.gameObject.SetActive(true);
+    }
+
+    public void EndDialogue() {
+        mDialogueCanvas.gameObject.SetActive(false);
+    }
+
+    public void SetAvatar(string name, string state = "") {
+
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # sftd-Unity
 sftd Unity backend
+
+## Assets
+- [Fantasy Wooden GUI : Free](https://assetstore.unity.com/packages/2d/gui/fantasy-wooden-gui-free-103811)
