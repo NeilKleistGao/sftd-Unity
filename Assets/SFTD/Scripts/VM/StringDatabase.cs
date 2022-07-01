1b6d10ad56f237b3fca268aceb9e0f2e11c97f76
NeilKleistGao <neilkleist@gmail.com>
2022-07-01 16:16:47 +0800

    Add csv parser

--- Assets/SFTD/Scripts/VM/StringDatabase.cs
+++ Assets/SFTD/Scripts/VM/StringDatabase.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using System.Text;
 
 
 [System.Serializable]
@@ -33,7 +34,35 @@ public class StringDatabase : MonoBehaviour {
     }
 
     public void LoadI18NFile (TextAsset pAsset) {
-        // TODO:
+        string content = Encoding.UTF8.GetString(pAsset.bytes);
+        string[] lines = content.Split('\n');
+        if (lines == null || lines.Length < 2) {
+            return;
+        }
+
+        string[] keys = lines[0].Split('\t');
+        int target = -1;
+        for (int i = 1; i < keys.Length; ++i) {
+            if (keys[i] == languageCode) {
+                target = i;
+                break;
+            }
+        }
+
+        if (target == -1) {
+            Debug.LogWarningFormat("Language code {0} missing in {1}", languageCode, pAsset.name);
+            return;
+        }
+
+        for (int i = 1; i < lines.Length; ++i) {
+            string[] values = lines[i].Split('\t');
+            if (values.Length < keys.Length) {
+                Debug.LogWarningFormat("Translation missing in {0}, line {1}", pAsset.name, i + 1);
+            }
+            else {
+                mTranslation.Add(values[0], values[target]);
+            }
+        }
     }
 
     public string GetString(string pName) {
